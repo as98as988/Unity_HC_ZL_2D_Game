f9b0683a8bec701da40dec88396d3a7ad5e9dd18
as98as988 <57396803+as98as988@users.noreply.github.com>
2019-11-21 21:32:30 +0800

    pipe生成

---
+++ New Unity Project/Assets/scripts/API.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("輸出");
+        Debug.Log("輸出訊息");
+       
+        
+        // 靜態成員：屬性的取得方式
+        print(Random.value);
+
+        // 練習取得PI
+        print(Mathf.PI);
+
+        print("隨機範圍:"+Random.Range(50.5f, 100.9f)
+            
+            );
+        Debug.Log(true);
+        Debug.LogWarning("警告");
+        Debug.LogError("錯誤");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 輸入.按鍵名稱
+        print(Input.inputString);
+
+        //練習偵測按S
+        if (Input.GetKeyDown("s"))
+        {
+            Debug.Log(Input.mousePosition);
+        }
+    }
+}
--- New Unity Project/Assets/scripts/GM.cs
+++ New Unity Project/Assets/scripts/GM.cs
@@ -10,7 +10,24 @@ public class GM : MonoBehaviour
         public int score;
         [Header ("最高分數")]
         public int scoreheight;
+    [Header("水管")]
+    public GameObject pipe;//G
 
+    /// <summary>
+    /// 生成水管功能
+    /// </summary>
+    public void SpawnPipe()
+    {
+        float y = Random.Range(-1.1f, 2.1f);
+        Vector3 pos = new Vector3(6f, y, 0);
+
+        Quaternion rot = new Quaternion(0, 0, 0, 0);
+        //因為有繼承類別才能簡寫 
+        //Object Instantiate(pipe);
+        Instantiate(pipe,pos,rot);
+
+    }
+    /// <param name="add"></param>
     public void AddScore(int add)
     {
 
@@ -24,5 +41,11 @@ public class GM : MonoBehaviour
     public void GameOver()
     {
 
+    }
+    private void Start()
+    {
+        // SpawnPipe();
+
+        InvokeRepeating("SpawnPipe", 0, 1.5f);
     }
 }
