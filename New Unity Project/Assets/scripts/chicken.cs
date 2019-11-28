ba85f3b55535f6198b3d75d46aa21298ab56b3fb
as98as988 <57396803+as98as988@users.noreply.github.com>
2019-11-28 21:17:33 +0800

    開起分數失敗

--- New Unity Project/Assets/scripts/chicken.cs
+++ New Unity Project/Assets/scripts/chicken.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class chicken : MonoBehaviour
 {
+    
+   
     [Header("跳躍高度"), Range(50, 2000)]
     public int jump = 100;
     [Header("是否死亡")]
     public bool dead;
+
+    public GameObject goScore, goGM;
     
     private void Jump()
     {
-        
+        //如果按下左鍵
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            print("按下左鍵");
+
+            //分數顯示
+            goScore.SetActive(true);
+            //GM顯示
+            goGM.SetActive(true);
+        }
     }
 
  
@@ -31,5 +44,9 @@ public class chicken : MonoBehaviour
        
     {
        
+    }
+    private void Update()
+    {
+        Jump();
     }
 }
---
+++ New Unity Project/Assets/scripts/practicebool.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class practicebool : MonoBehaviour
+{
+    public bool open;
+   public int score;
+    public int hit;
+    void Start()
+    {
+        //判斷是 if(switch)
+        //語法
+        //(布林值){當if()布林值為true執行這裡}
+
+        if(true)
+        {
+            print("嗨");
+        }
+    }
+
+    
+   private void Update()
+    {
+        if (open)
+        {
+            print("開門");
+        }
+        else
+        {
+            print("關門");
+        }
+
+        if(score>=60)
+        {
+            print("及格");
+
+        }
+        else if(score>=40)
+        {
+            print("可補考");
+        }
+        else if(score>=10)
+        {
+            print("付錢補考");
+        }
+        else
+        {
+            print("不及格");
+        }
+
+        if(hit>=150)
+        {
+            print("攻擊10倍");
+        }
+        else if(hit>=100)
+        {
+            print("攻擊5倍");
+
+        }
+        else if(hit>=50)
+        {
+            print("攻擊2倍");
+        }
+        else 
+        {
+            print("自爆");
+        }
+    }
+}
