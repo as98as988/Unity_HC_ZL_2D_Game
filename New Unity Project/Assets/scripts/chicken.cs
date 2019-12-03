f0cbc9c4aceaef51cd61cb00e6a4e73fd0826a22
as98as988 <57396803+as98as988@users.noreply.github.com>
2019-12-03 20:02:36 +0800

    jump

--- New Unity Project/Assets/scripts/chicken.cs
+++ New Unity Project/Assets/scripts/chicken.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class chicken : MonoBehaviour
 {
+
     
-   
+
     [Header("跳躍高度"), Range(50, 2000)]
-    public int jump = 100;
+    public int jump = 800;
     [Header("是否死亡")]
     public bool dead;
 
     public GameObject goScore, goGM;
+    [Header("鋼體")]
+
+    public Rigidbody2D r2d;
+
     
     private void Jump()
     {
@@ -24,9 +29,21 @@ public class chicken : MonoBehaviour
             goScore.SetActive(true);
             //GM顯示
             goGM.SetActive(true);
-        }
-    }
 
+            r2d.gravityScale = 2;
+            //鋼體重力=1
+            r2d.Sleep();
+            //鋼體睡覺
+            r2d.AddForce(new Vector2(0, jump));
+            //鋼體.增加推力(2維向量)
+
+        }
+        //velocity 鋼體加速度(x,y)左右x上下y
+        print(r2d.velocity);
+       //鋼體 設定角度
+        r2d.SetRotation(8*r2d.velocity.y);
+    }
+    
  
 
 
@@ -48,5 +65,6 @@ public class chicken : MonoBehaviour
     private void Update()
     {
         Jump();
+        
     }
 }
